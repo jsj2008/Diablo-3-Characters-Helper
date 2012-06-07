5fe468014584f8b85da32672b262aca40dfd3b61
geoffreyantoine <geoffrey.antoine@neoweb.fr>
2012-06-07 17:11:47 +0200

    pouet

---
+++ chooseJobViewController.h
@@ -0,0 +1,13 @@
+//
+//  chooseJobViewController.h
+//  Diablo 3 Characters Helper
+//
+//  Created by Geoffrey Antoine on 07/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface chooseJobViewController : UIViewController
+
+@end
---
+++ chooseJobViewController.m
@@ -0,0 +1,43 @@
+//
+//  chooseJobViewController.m
+//  Diablo 3 Characters Helper
+//
+//  Created by Geoffrey Antoine on 07/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "chooseJobViewController.h"
+
+@interface chooseJobViewController ()
+
+@end
+
+@implementation chooseJobViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
